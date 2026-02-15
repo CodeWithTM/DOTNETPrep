60030cc807ce54e9d4e9f0185f360bd6b6b18a75
CodeWithTM <tukaram.murdekar@gmail.com>
2026-02-15 17:48:41 +0530

    inner ex

---
+++ DOTNetBasics/ExceptionHandling/InnerExceptionExample.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+
+    //Inner Exception in C#
+    /*
+    An inner exception is the real/original exception that caused a new exception.
+
+    You usually wrap exceptions when:
+
+    you want to add more context
+
+    but still keep the original root cause
+    */
+
+    internal class InnerExceptionExample
+    {
+        public static void Main2()
+        {
+            try
+            {
+                Case1_InnerNull(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("===== CASE 1 =====");
+                PrintException(ex);
+            }
+
+            Console.WriteLine();
+
+            try
+            {
+                Case2_InnerFilled(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("===== CASE 2 =====");
+                PrintException(ex);
+            }
+        }
+
+        static void Case1_InnerNull(int number)
+        {
+            try
+            {
+                int result = 10 / number;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Case1: Failed while dividing"); // in this case inner = null
+            }
+        }
+
+        static void Case2_InnerFilled(int number)
+        {
+            try
+            {
+                int result = 10 / number;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Case2: Failed while dividing", ex); // inner = ex (i.e. attempt to divide by zero)
+            }
+        }
+
+        static void PrintException(Exception ex)
+        {
+            Console.WriteLine("Message: " + ex.Message);
+            Console.WriteLine("Type: " + ex.GetType().Name);
+
+            if (ex.InnerException == null)
+            {
+                Console.WriteLine("InnerException: NULL");
+            }
+            else
+            {
+                Console.WriteLine("InnerException Message: " + ex.InnerException.Message);
+                Console.WriteLine("InnerException Type: " + ex.InnerException.GetType().Name);
+            }
+        }
+    }
+}
--- DOTNetBasics/ExceptionHandling/Program.cs
+++ DOTNetBasics/ExceptionHandling/Program.cs
@@ -4,62 +4,114 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
 
-
-        }
-
-        // basic example of exception handling
-        public static void Divide(int a, int b)
-        {
             try
             {
-                int result = a / b;
-                Console.WriteLine($"Result: {result}");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Cannot divide by zero. Please provide a non-zero denominator.");
-                Console.WriteLine($"Error details: {ex.Message}");
+                // This is unhanled exception and app will crashhhh!
+                BaseMethod(4);
+
+                AnotherImpMethodThatneedsToBeExecuted();
+
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An unexpected error occurred.");
-                Console.WriteLine($"Error details: {ex.Message}");
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
+
         }
 
-        // i want it to nested function calls with exception handling
-        public static void OuterFunction()
+        private static int BaseMethod(int index)
         {
+            //return ChildMethod(index);
+
+            //add exception handling in child method and see the difference
             try
             {
-                Console.WriteLine("Entering OuterFunction.");
-                InnerFunction();
-                Console.WriteLine("Exiting OuterFunction.");
+                
+
+                int result = ChildMethodWithExceptionHandling(index);
+
+                int.Parse("a");
+
+                return result;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An error occurred in OuterFunction.");
-                Console.WriteLine($"Error details: {ex.Message}");
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+
+                //throw; // Rethrow the exception to be handled by the caller if needed
+
+
+
+                //throw vs throw ex; => throw will preserve the original stack trace,
+                //while throw ex; will reset the stack trace to the point where the exception is rethrown.
+                //It's generally recommended to use throw without specifying the exception variable to maintain the original stack trace for better debugging.
+
+                //then why throw ex exists? 
+                //Because C# allows you to throw any exception object, not only the one you just caught.
+                //e.g. Throwing a different exception object - as below
+
+                throw new InvalidOperationException("Invalid operation!", ex); //So in this case "Index out of range" exception will be the inner exception of "Invalid operation!" exception.
+                                                                               //This way you can provide more context about the error while still preserving the original exception details.
+                //return -1;
             }
+
         }
-        public static void InnerFunction()
+
+        private static int ChildMethod(int index)
         {
-            try
-            {
-                Console.WriteLine("Entering InnerFunction.");
-                // Simulate an error
-                throw new InvalidOperationException("Something went wrong in InnerFunction.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred in InnerFunction.");
-                Console.WriteLine($"Error details: {ex.Message}");
-                // Rethrow the exception to be handled by the outer function
-                throw;
-            }
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            return arr[index];
         }
+
+        private static void AnotherImpMethodThatneedsToBeExecuted()
+        {
+            Console.WriteLine("This is another important method that needs to be executed.");
+        }
+
+
+        private static int ChildMethodWithExceptionHandling(int index)
+        {
+            //try
+            //{
+                int[] arr = { 1, 2, 3, 4, 5 };
+                return arr[index];
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine("Index is out of range. Please provide a valid index.");
+            //    Console.WriteLine($"Error details: {ex.Message}");
+            //    //return -1; // Return a default value or handle as needed
+            //
+            //    throw; // Rethrow the exception to be handled by the caller if needed
+            //
+            //
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("An unexpected error occurred.");
+            //    Console.WriteLine($"Error details: {ex.Message}");
+            //    return -1; // Return a default value or handle as needed
+            //}
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
