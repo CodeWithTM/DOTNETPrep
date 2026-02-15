70aaea75545108fccb8c841c822b4a5613436f42
CodeWithTM <tukaram.murdekar@gmail.com>
2026-02-15 19:35:31 +0530

    inner ex nested

--- DOTNetBasics/ExceptionHandling/ExceptionExamples.cs
+++ DOTNetBasics/ExceptionHandling/ExceptionExamples.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ExceptionHandling
 {
+
+    //A method either returns a value OR throws an exception.
+    //But the compiler still wants proof that every possible path either returns or throws.
     internal class ExceptionExamples
     {
         // basic example of exception handling
@@ -15,6 +18,15 @@ namespace ExceptionHandling
             {
                 int result = a / b;
                 Console.WriteLine($"Result: {result}");
+
+                GetUserName(1234);
+
+                if(TryGetUsername(1234, out string name))
+                {
+                    Console.WriteLine(name);
+                }
+
+
             }
             catch (DivideByZeroException ex)
             {
@@ -57,6 +69,47 @@ namespace ExceptionHandling
                 Console.WriteLine($"Error details: {ex.Message}");
                 // Rethrow the exception to be handled by the outer function
                 throw;
+            }
+        }
+
+        public static string GetUserName(int userId)
+        {
+            try
+            {
+                if (userId == 1)
+                    return "Alice";
+
+                throw new Exception($"User with id {userId} not found");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DB error");
+
+
+                //return null; // this is a return but it doesn't make sense to return null here because we don't know if userId was valid or if there was a DB error. We should throw an exception instead.
+
+                throw new Exception("Failed to load user from DB!", ex);
+            }
+        }
+
+        public static bool TryGetUsername(int userId, out string name)
+        {
+            try
+            {
+                if (userId == 1)
+                {
+                    name = "Alice";
+                    return true;
+                }
+
+                name = null;
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                name = null;
+                return false;
             }
         }
     }
